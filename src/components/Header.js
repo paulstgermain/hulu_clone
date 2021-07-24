085f8b05beb452dc2395fe4eeefeea4c3043adde
paulstgermain <paulg.stgermain@gmail.com>
2021-07-24 16:26:58 -0400

    moves images

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
+import logo from '../img/logo.png';
 
 const Header = () => {
     return (
+        <>
         <h4>Try up to one month free</h4>
+        <img src={logo} alt="hulu logo" class="logo" />
+        </>
     )
 }
 
