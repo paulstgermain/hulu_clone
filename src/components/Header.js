08f576800627ba4b5184e885a0b995a3ef3725d4
paulstgermain <paulg.stgermain@gmail.com>
2021-07-24 16:37:26 -0400

    adds header content, basic styles + font

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,10 @@ const Header = () => {
         <>
         <h4>Try up to one month free</h4>
         <img src={logo} alt="hulu logo" class="logo" />
+        <p class='header-txt-1'>Watch thousands of TV shows and movies.</p>
+        <p class='header-txt-2'>HBO Max™, SHOWTIME®, CINEMAX® and STARZ® available as add-ons.</p>
+        <button class='btn btn-cta'>Start your free trial</button>
+        <p class='legal-txt'>Free trial for new & eligible returning subscribers only.</p>
         </>
     )
 }
