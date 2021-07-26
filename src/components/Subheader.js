cb6ba187f19d5cada6f5cdfe4a92e0367c414146
paulstgermain <paulg.stgermain@gmail.com>
2021-07-26 16:59:28 -0400

    implements css grid + gradient background

--- src/components/Subheader.js
+++ src/components/Subheader.js
@@ -8,11 +8,11 @@ const Subheader = () => {
             <div>
                 <h4>Bundle with any Hulu Plan & Save</h4>
                 <h3>Get Hulu, Disney+, and ESPN+.</h3>
-                <h4 className='legal-txt'>Details</h4>
+                <a href='#' className='sub-link'>Details</a>
             </div>
             <div>
-            <button className='btn'>Get Bundle</button>
-            <a href='#' className='legal-txt'>Terms apply</a>
+            <a href='#' className='btn btn-outline'>Get Bundle</a>
+            <a href='#' className='sub-link'>Terms apply</a>
             </div>
         </StyledSubheader>
     )
@@ -20,20 +20,29 @@ const Subheader = () => {
 
 const StyledSubheader = styled.section`
     width: 100%;
-    display: flex;
-    padding: 50px 0 15px 0;
+    display: grid;
+    grid-template-columns: 2fr 4fr 2fr;
+    gap: 40px;
+    align-items: center;
+    padding: 30px 40px;
+    background: linear-gradient(
+        318.68deg,
+        #0f495c 0%,
+        #0d3640 49.72%,
+        #08141f 100%
+    );
 
     img {
         width: 300px;
         height: 65px;
-        padding: 0 10px 0 10px;
+
     }
 
     a {
         display: block;
     }
 
-    button {
+    .btn-outline {
         background: none;
         border: 2px solid #FFFFFF;
         color: #FFFFFF;
