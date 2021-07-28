d845f252c81ef5352ba2d0b12566813cbad1fa93
paulstgermain <paulg.stgermain@gmail.com>
2021-07-28 01:29:51 -0400

    adds Catagories section

---
+++ src/components/Catagories.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Catagories = () => {
+    return (
+        <StyledCatagories>
+            <h1>Catagories Section</h1>
+        </StyledCatagories>
+    )
+}
+
+const StyledCatagories = styled.section`
+
+`
+
+export default Catagories;
