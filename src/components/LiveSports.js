9612b265b8ed1704351037790fbe09ab7dae56f3
paulstgermain <paulg.stgermain@gmail.com>
2021-08-01 23:20:09 -0400

    styles section content, starts media queries

--- src/components/LiveSports.js
+++ src/components/LiveSports.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 import liveSportsBg from '../img/live-sports.jpg';
+import liveSportsBgSm from '../img/live-sports-small.jpg';
 import liveSportsLogo1 from '../img/live-sports-logo-1.png';
 import liveSportsLogo2 from '../img/live-sports-logo-2.png';
 import liveSportsLogo3 from '../img/live-sports-logo-3.svg';
 import liveSportsLogo4 from '../img/live-sports-logo-4.png';
+import networkLogoBg from '../img/network-logo-bg.png';
 
 const LiveSports = () => {
     return (
@@ -32,7 +34,41 @@ const LiveSports = () => {
 }
 
 const StyledLiveSports = styled.section`
+    background: url(${liveSportsBg}) no-repeat center center / cover;
+    height: 800px;
+    position: relative;
 
+    .live-sports-content {
+        position: absolute;
+        top: 160px;
+        left: 100px;
+        max-width: 550px;
+    }
+
+    .live-sports-logos {
+        width: 300px;
+        margin-top: 40px;
+        display: flex;
+        align-self: center;
+        justify-content: space-between;
+
+        div {
+            background: url(${networkLogoBg}) no-repeat center center / cover;
+            height: 60px;
+            width: 60px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            img {
+                width: 50px;
+            }
+        }
+    }
+
+    @media(max-width: 736px) {
+        background: url(${liveSportsBgSm}) no-repeat center center / cover;
+    }
 `
 
 export default LiveSports;
